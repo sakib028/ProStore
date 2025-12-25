c856b13c8381ca9b2453d6d595c1f864594fbcbd
sakib028 <sakibhossainkhankaptai007@gmail.com>
2025-12-26 00:25:53 +0600

    ADD TO Cart again

--- lib/actions/CartAction.ts
+++ lib/actions/CartAction.ts
@@ -1,8 +1,12 @@
 import { CardItem } from "@/types";
 
 export async function addItemToCart(item: CardItem) {
-  return {
-    success: true,
-    message: "Item added to cart successfully",
-  };
+  try {
+    return {
+      success: true,
+      message: "Item added to cart successfully",
+    };
+  } catch (error) {
+    return {};
+  }
 }
